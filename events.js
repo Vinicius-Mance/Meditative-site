ec5a7b601cc715293d022383fae5358025fe9000
VInicius-Mance <vinimanceprof@gmail.com>
2020-10-26 20:56:38 -0300

    Detection and music update
    All keys and mouse movement are being detected, and music starts playing as long as you don't touch anything

--- events.js
+++ events.js
@@ -1,31 +1,64 @@
 //pegar o input a ser pressionado
-let teste = document.querySelector('*');
-teste.addEventListener("keypress", stopAndLog);
-teste.addEventListener("click", stopAndLog);
-teste.onmousemove = function(evt){stopAndLog(evt);}
-window.onload = function(){infiniteReset();}
-window.onscroll = function(evt){stopAndLog(evt);}
-teste.onmouseover = function(evt){stopAndLog(evt);}
-teste.onmouseout = function(){clearInterval(timer);}
-let timer = "";
-function infiniteReset(){
-  timer = setInterval(startTimer,3000);
+window.onload = function(){initiateTimer();}
+
+window.onkeydown = function(evt){reset(evt);}
+
+window.onmousemove = function(evt){reset(evt);}
+
+window.onmousedown = function(evt){reset(evt);}
+
+window.onscroll = function(evt){reset(evt);}
+
+window.onmouseover = function(evt){
+    mouseOnScreen = true;
+    reset(evt);
 }
 
-function stopAndLog(evt){
-  resetTimer();
-  clearInterval(timer);
-  infiniteReset();
-  console.log(evt);
+window.onmouseout = function(evt){
+    mouseOnScreen = false;
+    reset(evt);
 }
 
-var timerDisplay = document.querySelector('.timer');
-var startTime;
-var updatedTime;
-var difference;
-var tInterval;
-var savedTime;
-var running = 0;
+function initiateTimer(){
+  timer = setInterval(startTimer, waiting);
+  musicTimer = setInterval(playMusic, waiting);
+}
+
+function reset(evt){
+  evt.preventDefault();
+  resetMusic();
+    if (mouseOnScreen === true) {
+      initiateTimer();
+    }
+    if (evt.button == 2) {
+      resetMusic();
+    }
+}
+
+ function playMusic() {
+    music.play();
+ }
+
+ function resetMusic(){
+   resetTimer();
+   music.pause();
+   music.currentTime = 0;
+   clearInterval(musicTimer);
+   clearInterval(timer);
+ }
+
+let timer;
+let musicTimer;
+let waiting = 3000;
+let music = document.getElementById('music');
+let mouseOnScreen;
+let timerDisplay = document.querySelector('.timer');
+let startTime;
+let updatedTime;
+let difference;
+let tInterval;
+let savedTime;
+let running = 0;
 
 function startTimer(){
   if(!running){
@@ -42,10 +75,10 @@ function getShowTime(){
   } else {
     difference =  updatedTime - startTime;
   }
-  var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((difference % (1000 * 60)) / 1000);
-  var milliseconds = Math.floor((difference % (1000 * 60)) / 100);
+  let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+  let milliseconds = Math.floor((difference % (1000 * 60)) / 100);
   hours = (hours < 10) ? "0" + hours : hours;
   minutes = (minutes < 10) ? "0" + minutes : minutes;
   seconds = (seconds < 10) ? "0" + seconds : seconds;
